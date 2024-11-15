9b4d31df32fe00feee470b71208fe49af268032f
Siddhesh Joshi <siddheshjoshi@Siddheshs-Laptop.local>
2024-11-15 18:46:33 +0530

    initial setup

---
+++ src/main/java/com/customer/experience/SpringBootJdbctemplateMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.customer.experience;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJdbctemplateMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJdbctemplateMysqlApplication.class, args);
+	}
+
+}
