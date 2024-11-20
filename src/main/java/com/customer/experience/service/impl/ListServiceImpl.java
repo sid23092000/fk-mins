ba2df3e8e0cb4a58580be6362d7871dad325c033
siddheshjoshi <joshisiddhesh28@gmail.com>
2024-11-20 18:04:08 +0530

    merge api fix

--- src/main/java/com/customer/experience/service/impl/ListServiceImpl.java
+++ src/main/java/com/customer/experience/service/impl/ListServiceImpl.java
@@ -82,20 +82,82 @@ public class ListServiceImpl implements ListService {
             Lists listAdded = listRepository.save(newList);
 
             // Retrieve all items associated with the original lists using the same IDs
+<<<<<<< Updated upstream
             List<Items> items = itemsRepository.findAllById(ids);
+=======
+            List<Items> items = itemRepository.findAllByListIdIn(ids);
+>>>>>>> Stashed changes
 
-            // Update the listId for all the items to point to the new merged list
-            int newListId = listAdded.getId();  // Get the ID of the newly saved list
+            List<Items> newCombinedItemsList = getItems(items, listAdded);
 
+<<<<<<< Updated upstream
             for (Items item : items) {
                 item.setListId(newListId);  // Update each item's listId to the new list's ID
             }
             itemsRepository.saveAll(items);
+=======
+            itemRepository.saveAll(newCombinedItemsList);
+>>>>>>> Stashed changes
 
         } catch (Exception e) {
             throw new Exception("Error while merging lists: " + e.getMessage());
         }
     }
+<<<<<<< Updated upstream
+=======
+
+    private static List<Items> getItems(List<Items> items, Lists listAdded) throws Exception {
+        if(items.isEmpty()) {
+            throw new Exception("No items found to merge list");
+        }
+        // Update the listId for all the items to point to the new merged list
+        int newListId = listAdded.getId();  // Get the ID of the newly saved list
+
+        List<Items> newCombinedItemsList = new ArrayList<>();
+        for (Items item : items) {
+            Items newItem = new Items();
+            newItem.setListId(newListId);  // Update each item's listId to the new list's ID
+            newItem.setName(item.getName());
+            newItem.setQuantity(item.getQuantity());
+            newCombinedItemsList.add(newItem);
+        }
+        return newCombinedItemsList;
+    }
+
+    @Override
+    public List<ListsDescDto> fetchLists(int userId) {
+        log.info("[fetchListModel] fetching list for the user = {}", userId);
+        List<Lists> lists = listRepository.findAllByUserId(userId);
+        return lists.stream().map(list -> new ListsDescDto(list.getId(), list.getName(), list.getDesc())).collect(Collectors.toList());
+    }
+
+    @Override
+    public ListItemsDetailsDto fetchListItems(int listId) {
+        log.info("[fetchListModel] fetching list items for the list = {}", listId);
+        List<Items> listItems;
+        try {
+            listItems = itemRepository.findAllByListId(listId);
+            if (listItems.isEmpty()) {
+                return new ListItemsDetailsDto();
+            }
+            ListItemsDetailsDto listItemsDetailsDto = new ListItemsDetailsDto();
+            listItemsDetailsDto.setId(listId);
+            ArrayList<ItemsDetailsDto> itemsDetailsDtoList = new ArrayList<>();
+            for (Items item : listItems) {
+                ItemsDetailsDto dto = new ItemsDetailsDto();
+                dto.setName(item.getName());
+                dto.setQuantity(item.getQuantity());
+                itemsDetailsDtoList.add(dto);
+            }
+
+            listItemsDetailsDto.setItems(itemsDetailsDtoList);
+            return listItemsDetailsDto;
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            throw e;
+        }
+    }
+>>>>>>> Stashed changes
 
 
 }
